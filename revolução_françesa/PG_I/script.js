cdb4899d2eb0022a6e8e540441b1f1c142e632c5
Kayque2678 <142501069+kayque2678@users.noreply.github.com>
2024-04-07 22:32:02 -0300

    Js-primeiro commit

---
+++ revolução_françesa/PG_I/script.js
@@ -0,0 +1,10 @@
+const botaoAbrir = document.querySelector("header > button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector("header nav")
+
+botaoAbrir.addEventListener('click',)
+
+function abrirMenu() {
+    body.classList.add("escurecer")
+}
